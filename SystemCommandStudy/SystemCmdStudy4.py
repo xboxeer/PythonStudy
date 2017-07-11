7e02858523ab0674afd96e5c6dc12029211186aa
Elendil <zy4182@sina.com>
2017-07-12 01:16:25 +0800

    MultiProcess Study

--- SystemCommandStudy/SystemCmdStudy4.py
+++ SystemCommandStudy/SystemCmdStudy4.py
@@ -1,3 +1,12 @@
 import subprocess
 ret=subprocess.getoutput('date')
 print(ret)
+
+ret1=subprocess.check_call('date')
+print(ret1)
+ret2=subprocess.getstatusoutput('date')
+print(ret2)
+ret3=subprocess.call(['date','-u'])
+print(ret3)
+ret4=subprocess.call('date -u',shell=True)
+print(ret4)
