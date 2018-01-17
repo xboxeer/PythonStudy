3fb9854476d956e1f9ca09e7c824c33c69358862
Elendil <zy4182@sina.com>
2018-01-18 01:36:47 +0800

    Climing Stairs Solution

---
+++ algorithm/ClimingStairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ways=0
+        ahead1=0
+        ahead2=0
+        for i in range(n+1):
+            if(i==1):
+                ahead1=1
+                ways=1
+            elif(i==2):
+                ahead2=2
+                ways=2
+            else:
+                ways=ahead1+ahead2
+                ahead1=ahead2
+                ahead2=ways
+        return ways
+test=Solution()
+print(test.climbStairs(10))
